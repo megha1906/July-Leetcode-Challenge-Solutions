3f5e881f93a661b9fa8e271e90d314cbd119753f
Megha Bansal <meghabansal135@gmail.com>
2020-07-05 18:35:29 +0530

    Update 5)Hamming DIstance.c

--- 5)Hamming DIstance.c
+++ 5)Hamming DIstance.c
@@ -6,8 +6,8 @@ int hammingDistance(int n1, int n2){
     int bits = 0; 
   
     while (x > 0) { 
-        bits += x & 1; 
-        x >>= 1; 
+       x=x&(x-1);
+        bits++;
     } 
   
     return bits; 
